a8626f3e3f4ab76f1577c7881c17aad2032ac915
Maksim Ponomarev <welilf@icloud.com>
2026-05-07 12:19:20 +0300

    add steps for allure

--- src/test/java/pages/SuccessfulFillFormPage.java
+++ src/test/java/pages/SuccessfulFillFormPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 import pages.components.CalendarComponent;
 import pages.components.ResultTableComponent;
 
@@ -29,79 +30,94 @@ public class SuccessfulFillFormPage {
 
     // Actions
 
+    @Step("Открываем страницу формы регистрации")
     public SuccessfulFillFormPage openPage() {
         open("/automation-practice-form");
         return this;
     }
 
+    @Step("Вводим имя: {value}")
     public SuccessfulFillFormPage typeFirstName (String value) {
         firstNameInput.setValue(value);
         return this;
     }
 
+    @Step("Вводим фамилию: {value}")
     public SuccessfulFillFormPage typeLastName (String value) {
         lastNameInput.setValue(value);
         return this;
     }
 
+    @Step("Вводим email: {value}")
     public SuccessfulFillFormPage typeUserEmail (String value) {
         userEmail.setValue(value);
         return this;
     }
 
+    @Step("Выбираем пол: {value}")
     public SuccessfulFillFormPage setGender (String value) {
         genderContainer.$(byText(value)).click();
         return this;
     }
 
+    @Step("Вводим номер телефона: {value}")
     public SuccessfulFillFormPage typeUserNumber (String value) {
         userNumber.setValue(value);
         return this;
     }
 
+    @Step("Устанавливаем дату рождения: {day} {month} {year}")
     public SuccessfulFillFormPage setDateOfBirth (String day, String month, String year) {
         dateOfBirth.click();
         calendar.setDate(day, month, year);
         return this;
     }
 
+    @Step("Выбираем предмет: {value}")
     public SuccessfulFillFormPage setSubject(String value) {
         subjectsInput.setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Выбираем хобби: {value}")
     public SuccessfulFillFormPage setHobby(String value) {
         hobbiesWrapper.$(byText(value)).click();
         return this;
     }
 
+    @Step("Загружаем изображение: {fileName}")
     public SuccessfulFillFormPage uploadPicture(String fileName) {
         uploadPicture.uploadFromClasspath(fileName);
         return this;
     }
 
+    @Step("Вводим адрес: {value}")
     public SuccessfulFillFormPage setAddress(String value) {
         currentAddressInput.setValue(value);
         return this;
     }
 
+    @Step("Выбираем штат: {value}")
     public SuccessfulFillFormPage setState(String value) {
         stateDropdown.scrollIntoView(true).click();
         $("#react-select-3-input").setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Выбираем город: {value}")
     public SuccessfulFillFormPage setCity(String value) {
         cityDropdown.click();
         $("#react-select-4-input").setValue(value).pressEnter();
         return this;
     }
 
+    @Step("Отправляем форму (Submit)")
     public SuccessfulFillFormPage submitForm() {
         submitButton.click();
         return this;
     }
 
+    @Step("Проверяем, что поле {key} имеет значение {value}")
     public SuccessfulFillFormPage checkField (String key, String value) {
         resultsTable.checkField(key, value);
         return this;
